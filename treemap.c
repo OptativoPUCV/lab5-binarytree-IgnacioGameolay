65c8d44fbc8a38df48534d487193ccf81eefcc35
Your Name <you@example.com>
2024-05-09 03:19:23 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -239,7 +239,7 @@ Pair * nextTreeMap(TreeMap * tree) {
 	//Si hay subarbol derecho
 	if (tree->current->right != NULL){
 		tree->current = tree->current->right;
-
+		printf("edad");
 		while(tree->current->left != NULL){
 				tree->current = tree->current->left;
 		}
